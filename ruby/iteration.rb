2cea649b3e7ebe2dfedd23a67bb3554a716d01cc
Reginad1 <reginadeangelis91@gmail.com>
2017-05-19 18:41:08 -0500

    release 1, add each and map to array and hash

--- ruby/iteration.rb
+++ ruby/iteration.rb
@@ -6,3 +6,35 @@ def complimenter
 end
 
 complimenter {|x, y| puts "You are a fabulous queen who has awesome #{x} and #{y}."}
+
+
+dogs = ["beagle", "lab", "maltipoo", "deshund"]
+
+p dogs
+dogs.each do |dogs|
+  puts dogs + " are very cute"
+end
+
+p dogs
+
+dogs.map! do |dogs|
+  puts dogs
+  dogs.chop
+end
+
+p dogs
+
+dog_names = {
+  dog_one: "Fluffy",
+  dog_two: "Mazzy",
+  dog_three: "Spot",
+  dog_four: "Lucy",
+}
+
+p dog_names
+
+dog_names.each do |dog_number, dog_name|
+  puts "#{dog_number} is named #{dog_name}"
+end
+
+p dog_names
