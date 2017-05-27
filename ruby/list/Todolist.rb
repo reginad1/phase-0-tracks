df769d29ae6700698a8c008ba94be09c8b72d76f
Reginad1 <reginadeangelis91@gmail.com>
2017-05-27 13:02:13 -0500

    add class and method to todolist to pass spec tests

--- ruby/list/Todolist.rb
+++ ruby/list/Todolist.rb
@@ -1,16 +1,30 @@
-class Todolist
+class TodoList
 
-  def create_list
+  def initialize(list)
+    @list = list
   end
 
-  def add_to_list
+  def get_items
+    @list
   end
 
-  def delete_from_list
+  def add_item(addition)
+    @list << addition
   end
 
-  def find_from_list
+  def delete_item(subtraction)
+    @list.delete(subtraction)
   end
 
+  def get_item(index)
+    @list[index]
+  end
 
 end
+
+list = TodoList.new(["do the dishes", "mow the lawn"])
+# p list.add_item("mop")
+
+# list.delete_item("do the dishes")
+
+# list.get_item(0)
