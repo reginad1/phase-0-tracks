32d549153fc45834e72b6b1fb8e9baf00148599d
Reginad1 <reginadeangelis91@gmail.com>
2017-05-30 06:18:04 -0500

    test file is working for class

--- ruby/game/wordgame_spec.rb
+++ ruby/game/wordgame_spec.rb
@@ -1,14 +1,14 @@
 require_relative 'wordgame'
 
-describe Wordgame do
-  let (:game) { Wordgame.new("hey") }
+describe WordGame do
+  let (:game) { WordGame.new("hey") }
 
-it "provides a new array with blanks" do
+it "provides a new array with blanks per letter in word" do
     expect(game.user_clue). to eq ["_", "_", "_"]
   end
 
 it "checks a letter from user two and updates array based on letter" do
-    expect(game.letter_checker('h')). to eq ["h", "_", "_"]
+    expect(game.letter_checker("h")). to eq ["h"]
   end
 
 end
