de1f309de19d73356220dad44a5dfdc804a75bd4
Reginad1 <reginadeangelis91@gmail.com>
2017-06-01 21:25:18 -0500

    add horse array and add color array

---
+++ js/javascript-intro-lab/data_structures.js
@@ -0,0 +1,9 @@
+var colors = ["iridescent purple", "lilac", "green with sparkles", "summer tomato red" ];
+
+var horses = ["Jill", "Jerry", "Java", "Jordach"];
+
+colors.push("orange yellow tie dye")
+
+horses.push("Jamboree")
+
+console.log(horses)
