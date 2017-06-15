d0e8bb795a4642cc5a89f840b72acd487e664fd7
reginad1 <reginadeangelis91@gmail.com>
2017-06-15 13:56:13 -0500

    fade in some list items, add list items, and remove list item

---
+++ web_dev/jquery/practice-basic-example.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+
+$(':header').addClass ('headline');
+$('li:lt(4)').hide().fadeIn(1500);
+
+$("#btn1").click(function (){
+
+	var selectedVal = $('select').val();
+
+	$('ul').append('<li>'+selectedVal+'</li>')
+
+})
+
+$('ul').on('click', 'li', function() {
+	$(this).remove();
+});
+
+
+});
