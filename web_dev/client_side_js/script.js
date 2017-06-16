2d2cc0e735ea4b3e83f016bd7e4b28ad522c23ce
reginad1 <reginadeangelis91@gmail.com>
2017-06-16 16:43:53 -0500

    color to list items

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,10 +1,18 @@
 console.log("The script is running!");
 
 function addBorder(event) {
-	event.target.style.border = "5px dotted darkblue";
+	event.target.style.border = "5px dotted #019962";
 }
 
 var els = document.getElementById('title')
 els.addEventListener("click", addBorder); 
 
+var lis = document.getElementsByTagName('strong')
+var li1 = lis[0]
+li1.style.color = "#019962";
 
+var li2 = lis[1]
+li2.style.color = "#019962";
+
+var li3 = lis[2]
+li3.style.color = "#019962";
