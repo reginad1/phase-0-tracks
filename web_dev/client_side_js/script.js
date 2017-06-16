7523dcdd3242c4e4fa80b3450c2f7f6a5098cb04
reginad1 <reginadeangelis91@gmail.com>
2017-06-16 15:32:20 -0500

    add event listener click h1 border

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -1,1 +1,10 @@
 console.log("The script is running!");
+
+function addBorder(event) {
+	event.target.style.border = "5px dotted darkblue";
+}
+
+var els = document.getElementById('title')
+els.addEventListener("click", addBorder); 
+
+
