9983d5c138bf7ac7fd0ec41aa774b22a171bad2b
reginad1 <reginadeangelis91@gmail.com>
2017-06-15 19:21:51 -0500

    add route to new erb template and route to make template dynamic

--- web_dev/sinatra_templating/app.rb
+++ web_dev/sinatra_templating/app.rb
@@ -17,11 +17,22 @@ get '/students/new' do
   erb :new_student
 end
 
+get '/about_student' do
+ @students = db.execute("SELECT DISTINCT campus FROM students")
+ @locations = db.execute("SELECT DISTINCT location FROM locations")
+ erb :about_student
+end 
+
 # create new students via
 # a form
 post '/students' do
   db.execute("INSERT INTO students (name, campus, age) VALUES (?,?,?)", [params['name'], params['campus'], params['age'].to_i])
   redirect '/'
 end
+
+get '/new_location' do 
+	db.execute("INSERT INTO locations (location) VALUES (?)", [params['location']])
+	redirect '/about_student'
+end 
 
 # add static resources
